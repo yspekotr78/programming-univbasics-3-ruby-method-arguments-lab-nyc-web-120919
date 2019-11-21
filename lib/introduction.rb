d2c7146f73c4ddb538898f68dbd29b679fa99b81
Yury Spektor <yspektor78@gmail.com>
2019-11-21 21:46:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,6 +13,7 @@ end
 introduction_with_language("Yury","Ruby")
 
 def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
